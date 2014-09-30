969feca0c78095d31f8445b4739758c5e5af50c7
Jonathan Herman <jdh11235@gmail.com>
2014-09-30 10:32:01 -0500

    display current set of notes

--- music/serialist_chromatic_octave.js
+++ music/serialist_chromatic_octave.js
@@ -6,11 +6,13 @@ var serialist_chromatic_octave = {
 	},
 
 	playNote: function(note) {
-		console.log(note);
+		var note_display = document.getElementById('serialist_chromatic_octave-note_display');
+		note_display.innerHTML = note_display.innerHTML + ' ' + note;
 		//TODO: interface with audiosynth.js
 	},
 
 	pattern: function() {
+		var note_display = document.getElementById('serialist_chromatic_octave-note_display');
 		var remaining_notes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 		//c to b, chromatic scale
 
@@ -24,7 +26,10 @@ var serialist_chromatic_octave = {
 				remaining_notes.splice(pos, 1);
 				setTimeout(randomNote, serialist_chromatic_octave.speed);
 			} else if (!serialist_chromatic_octave.stop) {
+				note_display.innerHTML = '';
 				serialist_chromatic_octave.pattern();
+			} else {
+				note_display.innerHTML = '';
 			}
 		}
 
