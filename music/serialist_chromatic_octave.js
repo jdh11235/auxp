b97532f1e337695b4d04e1ed3f739380cf2f9921
Jonathan Herman <jdh11235@gmail.com>
2014-09-30 10:02:04 -0500

    fixed bug on repeat (passing speed value)

--- music/serialist_chromatic_octave.js
+++ music/serialist_chromatic_octave.js
@@ -1,7 +1,8 @@
 var serialist_chromatic_octave = {
 	start: function(speed) {
 		this.stop = false;
-		this.pattern(speed);
+		this.speed = speed;
+		this.pattern();
 	},
 
 	playNote: function(note) {
@@ -9,9 +10,9 @@ var serialist_chromatic_octave = {
 		//TODO: interface with audiosynth.js
 	},
 
-	pattern: function(speed) {
-		var remaining_notes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-		//c to c, chromatic scale
+	pattern: function() {
+		var remaining_notes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+		//c to b, chromatic scale
 
 		function randomNote() {
 			if (remaining_notes[0] !== undefined) {
@@ -21,13 +22,13 @@ var serialist_chromatic_octave = {
 				serialist_chromatic_octave.playNote(note);
 
 				remaining_notes.splice(pos, 1);
-				setTimeout(randomNote, speed);
+				setTimeout(randomNote, serialist_chromatic_octave.speed);
 			} else if (!serialist_chromatic_octave.stop) {
-				setTimeout(serialist_chromatic_octave.pattern, speed); //bug
+				serialist_chromatic_octave.pattern();
 			}
 		}
 
 		serialist_chromatic_octave.playNote(0);
-		randomNote();
+		setTimeout(randomNote, serialist_chromatic_octave.speed);
 	}
 };
